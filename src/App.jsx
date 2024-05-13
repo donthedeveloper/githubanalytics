fddf88127de5e294847829930fd7cdbbc0ed5201
TeamRocketStudify <TeamRocketStudify@gmail.com>
2024-05-12 20:01:27 -0700

    added placeholder text for the Repos page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./layouts/Layout/Layout";
 import Home from "./components/Home/Home";
--- src/components/Repos/Repos.jsx
+++ src/components/Repos/Repos.jsx
@@ -5,6 +5,11 @@ function Repos() {
   return (
     <>
       <h1>Repos</h1>
+      <p>This is a placeholder. This page should show a list of repos from the logged in user.</p>
+      <p>To see a specific Gitgraph, you'll have to click on the repo.</p>
+      <br />
+      <br />
+      <br />
       <GitgraphComponent />
     </>
   )
