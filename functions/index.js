dfb7be14deb61d0b23c11f9179139bfc3ee2c973
Mark Baek <mkbk189@gmail.com>
2024-05-23 12:31:03 -0700

    initialized Firebase CLI and imported modules required to use Firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+// Main source file for Cloud Functions code
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+const { logger } = require("firebase-functions");
+const { onRequest } = require("firebase-functions/v2/https");
+const { onDocumentCreated } = require("firebase-functions/v2/firestore");
+
+// The Firebase Admin SDK to access Firestore.
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+initializeApp();
